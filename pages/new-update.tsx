c8a81c5e57188dd151a9a704a2a167891fbb01f9
wwsalmon <wwsamson@yahoo.com>
2020-11-28 01:09:06 -0500

    fix seo and redirects for sign in and new update pages

--- pages/new-update.tsx
+++ pages/new-update.tsx
@@ -3,6 +3,9 @@ import {format} from "date-fns";
 import axios from "axios";
 import {useRouter} from "next/router";
 import EditUpdate from "../components/EditUpdate";
+import {NextSeo} from "next-seo";
+import {GetServerSideProps} from "next";
+import {getSession} from "next-auth/client";
 
 export default function NewUpdate() {
     const router = useRouter();
@@ -30,6 +33,11 @@ export default function NewUpdate() {
 
     return (
         <div className="max-w-4xl mx-auto px-4">
+            <NextSeo
+                title="Post new update | Updately"
+                description="Post a new update to share with your followers."
+            />
+
             <h1 className="up-h1">New update</h1>
 
             <EditUpdate
@@ -48,3 +56,16 @@ export default function NewUpdate() {
         </div>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const session = await getSession(context);
+
+    if (!session) {
+        context.res.setHeader("location", "/sign-in");
+        context.res.statusCode = 302;
+        context.res.end();
+        return {props: {}};
+    }
+
+    return {props: {}};
+};
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -2,10 +2,15 @@ import {GetServerSideProps} from "next";
 import {getSession, signIn} from "next-auth/client";
 import {FaGoogle} from "react-icons/fa";
 import React from "react";
+import {NextSeo} from "next-seo";
 
 export default function SignIn() {
     return (
         <div className="max-w-sm px-4 mx-auto">
+            <NextSeo
+                title="Sign in to Updately"
+                description="Updately is a social platform for daily updates. Share updates with friends for social accountability and goal-setting"
+            />
             <h1 className="up-h1">Sign in</h1>
             <div className="content my-4">
                 <p>Sign in to post updates and follow other users.</p>
