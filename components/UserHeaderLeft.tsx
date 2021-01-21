b6d90ea883751edd89e7a2be2060728318ab7c25
Samson Zhang <wwsamson@yahoo.com>
2021-01-21 14:27:55 -0700

    add user join date on profile, close #32

--- components/UserHeaderLeft.tsx
+++ components/UserHeaderLeft.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {User} from "../utils/types";
+import {format} from "date-fns";
 
 export default function UserHeaderLeft({pageUser, userData}: { pageUser: User, userData: User }) {
     return (
@@ -9,8 +10,9 @@ export default function UserHeaderLeft({pageUser, userData}: { pageUser: User, u
             </div>
             <div>
                 <h1 className="up-h1">{pageUser.name}</h1>
+                <p className="my-2">Joined {format(new Date(pageUser.createdAt), "MMMM d, yyyy")}</p>
                 {userData && pageUser.following.includes(userData._id) && (
-                    <p className="text-xs mt-2 bg-black rounded-md p-2 text-white inline-block">Follows you</p>
+                    <p className="text-xs bg-black rounded-md p-2 text-white inline-block">Follows you</p>
                 )}
             </div>
         </div>
