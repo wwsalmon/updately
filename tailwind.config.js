236cf13a9f25c9df1e1bbc205bcefed1d734ff27
Laura Gao <lauragao86868@gmail.com>
2021-07-11 11:40:22 -0400

    dark mode update page styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     ],
     theme: {
         extend: {},
+        colors: {
+            transparent: 'transparent',
+            black: '#000',
+            white: '#fff',      
+            gray: {
+              100: '#f3f4f6',
+              900: '#121212',
+            },
+            red: {
+                500: '#ef4444',
+            },
+            blue: {
+                300: '#93c5fd',
+            }
+        },
     },
     variants: {
         extend: {
