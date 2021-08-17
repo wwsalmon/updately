d89d4f1e1f88533942d4dad1e6d4b98a6da1d6f5
Amy Li <69370988+ameletto@users.noreply.github.com>
2021-08-16 23:25:39 -0400

    Update NavbarItem.tsx

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NavbarItem({icon, text, href, selected}: { icon: ReactNode, text: string, href: string, selected: boolean }) {
     return (
         <Link href={href}>
-            <a className={`flex items-center px-6 h-full w-full sm:w-auto ${selected ? "border-b-2 border-black dark:border-white" : ""}`}>
+            <a className={`flex items-center px-6 h-full w-1/3 sm:w-auto ${selected ? "border-b-2 border-black dark:border-white" : ""}`}>
                 <div className={selected ? "" : "opacity-75"}>
                     {icon}
                 </div>
