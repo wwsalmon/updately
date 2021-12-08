5e29eda1ae0d9541f1af921e24adf40758992d8f
Samson Zhang <wwsamson@yahoo.com>
2021-12-07 17:47:47 -0800

    fix pagination bar back to 10

--- components/PaginationBar.tsx
+++ components/PaginationBar.tsx
@@ -9,12 +9,12 @@ export default function PaginationBar({page, count, label, setPage}: {
     return (
         <>
             <p className="opacity-50 mt-16">
-                Showing {label} {(page - 1) * 20 + 1}
-                -{(page < Math.floor(count / 20)) ? page * 20 : count} of {count}
+                Showing {label} {(page - 1) * 10 + 1}
+                -{(page < Math.floor(count / 10)) ? page * 10 : count} of {count}
             </p>
             {count > 20 && (
                 <div className="opacity-50 mt-4">
-                    {Array.from({length: Math.ceil(count / 20)}, (x, i) => i + 1).map(d => (
+                    {Array.from({length: Math.ceil(count / 10)}, (x, i) => i + 1).map(d => (
                         <button
                             className={"py-2 px-4 rounded-md mr-2 " + (d === page ? "opacity-50 cursor-not-allowed bg-gray-100 dark:bg-opacity-20" : "")}
                             onClick={() => setPage(d)}
