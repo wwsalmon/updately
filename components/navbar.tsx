03c1d0955fcc09dc0a6673ea3f0611d5f5f928fc
Samson Zhang <wwsamson@yahoo.com>
2022-03-21 10:41:19 -0700

    fix navbar responsiveness (close #45)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -110,7 +110,7 @@ export default function Navbar() {
             <div className="w-full sticky mb-8 top-0 z-30 bg-white dark:bg-gray-900">
                 <div className="max-w-7xl mx-auto h-16 flex items-center px-4">
                     <Link href="/"><a><img src="/logo.svg" className="h-12"/></a></Link>
-                    <div className="flex h-16 bg-white dark:bg-gray-900 fixed bottom-0 left-0 w-full sm:ml-8 sm:w-auto sm:relative sm:h-full">
+                    <div className="flex h-16 bg-white dark:bg-gray-900 fixed bottom-0 left-0 w-full md:ml-8 md:w-auto md:relative md:h-full">
                         {session && (
                             <NavbarItem icon={<FiHome/>} text="Feed" href="/" selected={router.route === "/"}/>
                         )}
@@ -120,7 +120,7 @@ export default function Navbar() {
                         )}
                     </div>
                     <div className="ml-auto flex items-center gap-x-4">                        
-                        {session && <Link href="/new-update"><a className="up-button small primary hidden sm:block">Post new update</a></Link>}
+                        {session && <Link href="/new-update"><a className="up-button small primary hidden lg:block">Post new update</a></Link>}
                         <NavbarDarkModeButton/>
                         {session ? (
                             <>
