be645c9b395e39062d0be8573a5b2f6266833da7
Samson Zhang <wwsamson@yahoo.com>
2022-08-28 23:38:39 -0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
         <div className="max-w-7xl mx-auto h-16 flex items-center px-4 mt-12">
-            <span className="dark:text-gray-300 opacity-50">Made with ♥ in New York by <a className="underline" href="https://www.linkedin.com/in/samsonzhangthesalmon/">Samson Zhang</a></span>
+            <span className="dark:text-gray-300 opacity-50">Made with ♥ by <a className="underline" href="https://twitter.com/wwsalmon">Samson Zhang</a>, <a
+                href="https://twitter.com/laurgao" className="underline">Laura Gao</a> and <a className="underline" href="https://github.com/wwsalmon/updately/graphs/contributors">others</a></span>
         </div>
     )
 }
