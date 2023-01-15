24214044b37b41015ae1849e6b8524655e8d14b6
Samson Zhang <wwsamson@yahoo.com>
2023-01-14 20:27:59 -0500

    banner dark mode fixes

--- components/UpBanner.tsx
+++ components/UpBanner.tsx
@@ -2,8 +2,7 @@ import React, {ReactNode} from "react";
 
 export default function UpBanner(props: {children: ReactNode, className?: string}) {
     return (
-        // #1e1e1e = "neutral 850"
-        <div className={"px-4 py-2 rounded-md border md:flex items-center bg-gray-50 dark:bg-[#1e1e1e] dark:border-neutral-600 " + (props.className || "")}>
+        <div className={"px-4 py-2 rounded-md border md:flex items-center bg-gray-50 dark:bg-neutral-900 dark:border-neutral-700 " + (props.className || "")}>
             {props.children}
         </div>
     )
